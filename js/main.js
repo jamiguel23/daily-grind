51ff5771501c93b00c0b0461d33f084f05bdcc5f
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2022-11-23 13:23:22 -0800

    commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+
+
+alert('Can you see this?')
+
+
+
+
+
+
+
+
+
+
+
+	//https://tinyurl.com/dynamic-html-checker
+  document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+		
+  document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);  
