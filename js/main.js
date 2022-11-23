4902a83ec35185e712490ff35a50fc09dca83ae4
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2022-11-23 13:31:38 -0800

    added day console log

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@
 alert('Can you see this?')
 
 
+let myDate = new Date();
+let day = myDate.getDay()
+let thisYear = myDate.getFullYear();
+
+console.log(day);
 
 
 
@@ -18,3 +23,5 @@ alert('Can you see this?')
   document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
 		
   document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);  
+
+  document.getElementById("this-year").innerHTML = thisYear;
